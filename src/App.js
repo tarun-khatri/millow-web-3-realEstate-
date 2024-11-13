d2e3acdd883a1ad9f86241aaf06fc52a2bf4e9f4
Tarun Khatri <ttkhatri559@gmail.com>
2024-11-13 19:06:43 +0530

    added frontend, connected to local blockchain

--- src/App.js
+++ src/App.js
@@ -14,10 +14,26 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+   
+    window.ethereum.om('accountChanged', async () => {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
+  }
+
+  useEffect(()=>{
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount}/>
       <div className='cards__section'>
 
         <h3>Welcome to Millow</h3>
